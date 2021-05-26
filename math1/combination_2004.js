41e660b2112ccf69df064f8407290f1b5c1c1e2b
JayoonKoo <rnwkdbs12@gmail.com>
2021-05-26 14:15:46 +0900

    백준 2004 품

--- math1/combination_2004.js
+++ math1/combination_2004.js
@@ -1,26 +1,38 @@
 // let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let input = ['17 12'];
+let input = ['25 12'];
 input = input[0].split(' ');
 const n = parseInt(input.shift());
 const m = parseInt(input.shift());
 
-const getTwoFive = (n) => {
-  let twoCount = 0;
-  let fiveCount = 0;
-  let twoDiv = 2;
-  let fiveDiv = 5;
-
-  while (n > fiveDiv) {
-    twoCount += parseInt(n / twoDiv);
-    
-    twoDiv *= 2;
-    if (twoDiv >= fiveDiv) {
-      fiveCount += parseInt(n /fiveDiv);
-      fiveDiv *= 5;
-    }
+const getFiveCount = (n) => {
+  let count = 0;
+  let div = 5;
+  while (n >= div) {
+    count += parseInt(n / div);
+    div *= 5;
   }
-  return [twoCount, fiveCount];
+  return count;
 };
 
-console.log(getTwoFive(n));
+const getTwoCount = (n) => {
+  let count = 0;
+  let div = 2;
+  while (n >= div) {
+    count += parseInt(n / div);
+    div *= 2;
+  }
+  return count;
+};
+
+// a / (b + c)
+let a = [getTwoCount(n), getFiveCount(n)];
+let b = [getTwoCount(n - m), getFiveCount(n - m)];
+let c = [getTwoCount(m), getFiveCount(m)];
+
+let two = a[0] - (b[0] + c[0]);
+let five = a[1] - (b[1] + c[1]);
+
+console.log(Math.min(two, five));
+
+
